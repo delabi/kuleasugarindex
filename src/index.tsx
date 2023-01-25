573f459ba06a5f7e8e7a40a941991704aa0e2176
Laban Bwire <bwire.laban@gmail.com>
2023-01-25 06:04:35 +0300

    log rocket addition

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import LogRocket from 'logrocket';
+
+LogRocket.init('kulea/sugarindex');
+
+LogRocket.identify("123456",{
+  name: "John Smith",
+  email: "john.smith@gmail.com",
+  age: 43,
+  favoriteColor: "blue",
+  currentSubscription: "professional-plan"
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
